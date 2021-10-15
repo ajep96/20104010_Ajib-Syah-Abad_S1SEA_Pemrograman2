d14b5f7564ab7c34c63a3fbc5bdd0c675409cb55
ajep96 <20104010@ittelkom-pwt.ac.id>
2021-10-15 09:40:48 +0700

    add modul0

---
+++ src/Modul0/Tipe.java
@@ -0,0 +1,24 @@
+package Modul0;
+
+public class Tipe {
+    public static void main(String[] args) {
+        long data1  = 767226531;
+        int data2   = 2235641;
+        short data3 = 714;
+        byte data4  = 34;
+        double data5    = 4.967;
+        float data6 = (float) 1.733;
+        char data7  = 'C';
+        boolean data8   = true;
+
+        System.out.println("Nilai Long \t\t: " + data1);
+        System.out.println("Nilai Int \t\t: " + data2);
+        System.out.println("Nilai Short \t: " + data3);
+        System.out.println("Nilai Byte \t\t: " + data4);
+        System.out.println("Nilai Double \t: " + data5);
+        System.out.println("Nilai Float \t: " + data6);
+        System.out.println("Nilai Char \t\t: " + data7);
+        System.out.println("Nilai Boolean \t: " + data8);
+
+    }
+}
