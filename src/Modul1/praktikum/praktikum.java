e8987254a1f35a1abb95e154cf4793898858e63c
ajep96 <20104010@ittelkom-pwt.ac.id>
2021-10-15 09:57:02 +0700

    add modul1

---
+++ src/Modul1/praktikum/praktikum.java
@@ -0,0 +1,7 @@
+package Modul1.praktikum;
+
+public class praktikum {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Modul1/tugas/tugas.java
@@ -0,0 +1,7 @@
+package Modul1.tugas;
+
+public class tugas {
+    public static void main(String[] args) {
+
+    }
+}
