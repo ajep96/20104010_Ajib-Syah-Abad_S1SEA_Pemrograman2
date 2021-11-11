c087b9855ef6763ba75abaf3e66421478fea0861
ajep96 <20104010@ittelkom-pwt.ac.id>
2021-11-11 15:14:49 +0700

    modul 6 percobaan praktikum

---
+++ src/Modul6/percobaan/empat/Child.java
@@ -0,0 +1,10 @@
+package Modul6.percobaan.empat;
+
+public class Child extends Parent{
+    int x;
+
+    public Child(){
+        super(); //penggunaan super harus di letakan pada statement bagian atas
+        x = 5;
+    }
+}
---
+++ src/Modul6/percobaan/satu/Bentuk.java
@@ -0,0 +1,5 @@
+package Modul6.percobaan.satu;
+
+public class Bentuk {
+    protected int p, l;
+}
