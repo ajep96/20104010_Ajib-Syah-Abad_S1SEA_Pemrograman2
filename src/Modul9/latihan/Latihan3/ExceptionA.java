0b4b3ebf62c05f6ff5c1060701f29d23d1f6387a
ajep96 <20104010@ittelkom-pwt.ac.id>
2021-12-19 14:13:59 +0700

    Latihan modul9

---
+++ src/Modul9/latihan/Latihan3/ExceptionA.java
@@ -0,0 +1,5 @@
+package Modul9.latihan.Latihan3;
+
+public class ExceptionA extends Exception{
+
+}
---
+++ src/Modul9/latihan/Latihan3/ExceptionB.java
@@ -0,0 +1,5 @@
+package Modul9.latihan.Latihan3;
+
+public class ExceptionB extends ExceptionA{
+
+}
