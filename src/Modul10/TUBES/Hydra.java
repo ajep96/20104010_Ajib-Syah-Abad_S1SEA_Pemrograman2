4fcf2f4e77a1d812bed53332bac683be304db185
ajep96 <20104010@ittelkom-pwt.ac.id>
2022-01-06 00:12:12 +0700

    update

--- src/Modul10/TUBES/Hydra.java
+++ src/Modul10/TUBES/Hydra.java
@@ -1,6 +1,6 @@
 package Modul10.TUBES;
 
-public class Hydra extends com.kelompok.tubes.Enemy {
+public class Hydra extends Enemy {
     public Hydra(int level){
         this.name = "Hydra";
         this.health = 50 * level;
@@ -9,5 +9,11 @@ public class Hydra extends com.kelompok.tubes.Enemy {
         this.defense = 20 * level;
         this.experience = 35 * level;
         this.level = level;
+    }
+
+    public String  IntroAlley() {
+        super.IntroAlley();
+        System.out.println("Hahaha, Human? its sound easy! \nHuman are weak");
+        return word;
     }
 }
